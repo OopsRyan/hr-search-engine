b23ed4d432f2c061f21b11dfb524f510976d9d9d
Ryan <qirun_chen@163.com>
2018-10-05 14:00:59 +0100

    add json writer

--- test-webservice/app/json/ReadJson.scala
+++ test-webservice/app/json/ReadJson.scala
@@ -3,15 +3,16 @@ package json
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
-class ReadJson {
+
+object JsonReader {
 
   val jsonString =
     """{
       | "teamName" : "Real Madrid FC",
       | "players" : [
       |   {
-        |   "name" : "Ronaldo",
-        |   "age" : 35
+      |   "name" : "Ronaldo",
+      |   "age" : 35
       |   },
       |   {
       |     "name" : "Modric",
@@ -25,15 +26,11 @@ class ReadJson {
       |}""".stripMargin
 
   val jValue: JsValue = Json.parse(jsonString)
-}
-
-
-object JsonRunner {
 
   def main(arg: Array[String]) = {
 
-    println(readTeamName(new ReadJson().jValue))
-    println(readNames(new ReadJson().jValue))
+    println(readTeamName(jValue))
+    println(readNames(jValue))
 
 //    implicit val locationReads: Reads[Location] = (
 //      (JsPath \ "lat").read[Double] and
@@ -53,18 +50,18 @@ object JsonRunner {
 //
 //    println(new ReadJson().jValue.as[Team])
 
-    macroTest(new ReadJson().jValue)
+    macroTest(jValue)
     
   }
 
   // Handy way
-  def macroTest(value: JsValue) = {
+  def macroTest(value: JsValue = jValue) = {
     implicit val playerReads: Reads[Player] = Json.reads[Player]
     implicit val locationReads = Json.reads[Location]
     implicit val teamReads = Json.reads[Team]
 
     val teams = value.as[Team]
-    println(teams)
+    teams
   }
 
   def readTeamName(value: JsValue) = {
@@ -85,12 +82,37 @@ object JsonRunner {
       case e: JsError => e.errors
     })
   }
-
-  case class Team(teamName: String, players: List[Player], location: Location)
-  case class Player(name: String, age: Int)
-  case class Location(lat: Double, long: Double)
+}
 
 
+case class Team(teamName: String, players: List[Player], location: Location)
+case class Player(name: String, age: Int)
+case class Location(lat: Double, long: Double)
 
 
+object JsonWriter {
+
+  def main(args: Array[String]): Unit = {
+    val json: JsValue = Json.obj(
+      "teamName" -> "Real Madrid FC",
+      "players" -> Json.arr(
+        Json.obj("name" -> JsString("Ronaldo"), "age" -> JsNumber(32)),
+        Json.obj("name" -> JsString("Modric"), "age" -> JsNumber(28)),
+        Json.obj("name" -> JsString("Bale"), "age" -> 23)
+      ),
+      "location" -> Json.obj("lat" -> 40.2233, "long" -> 2.3212)
+    )
+
+    println(json.toString())
+
+    macroTest(JsonReader.macroTest())
+  }
+
+  def macroTest(teams: Team) = {
+    implicit val playerWrites = Json.writes[Player]
+    implicit val locationWrites = Json.writes[Location]
+    implicit val positionWrites = Json.writes[Team]
+
+    println(Json.prettyPrint(Json.toJson(teams)))
+  }
 }
